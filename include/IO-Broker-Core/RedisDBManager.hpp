05cac9fb9babfd1e6cd9c8b091b85f62bb8a31e9
jitanshuraut <jayeshastha@gmail.com>
2024-10-18 22:51:35 +0530

    Version 1.0

---
+++ include/IO-Broker-Core/RedisDBManager.hpp
@@ -0,0 +1,26 @@
+#ifndef REDIS_DB_MANAGER_HPP
+#define REDIS_DB_MANAGER_HPP
+
+#include <iostream>
+#include <vector>
+#include <hiredis/hiredis.h>
+#include <string>
+
+class RedisDBManager {
+public:
+    RedisDBManager(const std::string& host = "127.0.0.1", int port = 6379);
+    ~RedisDBManager();
+
+    bool connect();
+    void publishCreateMessage(const std::string& dbFileName, const std::string& sqlStructure, const std::vector<std::string>& channels);
+    void publishInsertMessage(const std::string& dbFileName, const std::string& dataToInsert, const std::vector<std::string>& channels);
+
+private:
+    redisContext* context;
+    std::string host;
+    int port;
+
+    bool publishMessage(const std::string& channel, const std::string& message);
+};
+
+#endif 
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "./Velocity-Bot/bot.cpp" 
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+
+int main() {
+    TestBOT tradingBot;
+    
+    try {
+        tradingBot.initialize();
+    } catch (const std::exception& e) {
+        std::cerr << "Error during initialization: " << e.what() << std::endl;
+        return 1; 
+    }
+
+    std::thread tradingThread([&tradingBot]() {
+        try {
+            tradingBot.run(); 
+        } catch (const std::exception& e) {
+            std::cerr << "Error during run: " << e.what() << std::endl;
+        }
+    });
+
+    std::this_thread::sleep_for(std::chrono::minutes(10));
+    tradingBot.stop(); 
+
+    if (tradingThread.joinable()) {
+        tradingThread.join();
+    }
+
+    return 0;
+}
